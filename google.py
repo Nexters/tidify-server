fda8b69045187389f4dcd5ca2d5fd8e9861cd5ae
minkj1992 <minkj1992@gmail.com>
2021-09-19 21:15:22 +0900

    test: google test login script 동작 확인

--- google.py
+++ google.py
@@ -10,13 +10,13 @@ import os
 from authlib.integrations.base_client import OAuthError
 from authlib.integrations.starlette_client import OAuth
 from fastapi import FastAPI
+from pydantic import typing
 from starlette.config import Config
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 
 _SECRET_KEY = hashlib.sha256(os.urandom(1024)).hexdigest()
-_CLIENT_SECRETS_FILE = "client_secret.json"
 
 oauth_client = FastAPI()
 oauth_client.add_middleware(SessionMiddleware, secret_key=_SECRET_KEY)
@@ -24,12 +24,26 @@ oauth_client.add_middleware(SessionMiddleware, secret_key=_SECRET_KEY)
 config = Config(".google.env")
 oauth = OAuth(config)
 oauth.register(
-        name='google',
-        server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-        client_kwargs={
-            'scope': 'openid email profile'
-        }
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
 )
+
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def _pretty(d):
+    pp.pprint(d)
+
+
+def _beautify_print(idx, help_text, raw_data: typing.Dict):
+    print(f'{idx}.{help_text} start #######################################')
+    _pretty(raw_data)
+    print(f'{idx}.{help_text} finished #######################################')
 
 
 @oauth_client.get('/')
@@ -47,19 +61,22 @@ async def homepage(request: Request):
 
 @oauth_client.route('/login')
 async def login(request: Request):
+    _beautify_print(1, "request for login", request.__dict__)
     redirect_uri = request.url_for('sign_in')
-    tmp = await oauth.google.authorize_redirect(request=request, redirect_uri=redirect_uri)
-    return tmp
+    return await oauth.google.authorize_redirect(request=request, redirect_uri=redirect_uri)
 
 
 @oauth_client.route('/sign_in')
 async def sign_in(request: Request):
+    _beautify_print(2, "redirect for sign_in", request.__dict__)
     try:
         token = await oauth.google.authorize_access_token(request)
+        _beautify_print(3, "redirect with token", token)
     except OAuthError as error:
         return HTMLResponse(f'<h1>{error.error}</h1>')
 
     user = await oauth.google.parse_id_token(request, token)
+    _beautify_print(4, "user info", dict(user))
     request.session['user'] = dict(user)
     return RedirectResponse(url='/')
 
@@ -73,4 +90,4 @@ async def logout(request: Request):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(oauth_client, host='127.0.0.1', port=8080)
+    uvicorn.run(oauth_client, host='127.0.0.1', port=8080, reload=True)
